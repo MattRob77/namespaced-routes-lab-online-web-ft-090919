a762d5f7b7b0e51b7c5f3685df05a6310bbf4dec
Matthew Robinson <matt_robinson1@yahoo.com>
2019-11-18 19:57:58 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preference_controller.rb
+++ app/controllers/admin/preference_controller.rb
@@ -1,4 +1,4 @@
-class Admin::PreferencesController < ApplicationController
+class Admin::PreferenceController < ApplicationController
   def index
     @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true, song_sort_order: "DESC", artist_sort_order: "DESC")
   end
